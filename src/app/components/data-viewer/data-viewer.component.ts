9c7a494689283653dc8f49b3a51ca1acd9be61f6
tcassembler <tcassembler@tcassemblerdeMacBook-Pro.local>
2018-12-23 06:58:51 +0800

    fix issue 66

--- src/app/components/data-viewer/data-viewer.component.ts
+++ src/app/components/data-viewer/data-viewer.component.ts
@@ -43,6 +43,7 @@ export class DataViewerComponent implements OnInit, OnChanges {
   public setCachedData = null;
   public hashCachedData = null;
   public selectedMap = {};
+  public showPagination = false;
 
   constructor(
     public dialogService: MatDialog,
@@ -164,12 +165,14 @@ export class DataViewerComponent implements OnInit, OnChanges {
       value: v
     })));
 
+    this.showPagination = false;
 
     if (type === 'list') {
       this.loadingPageData = true;
       this.redisService.call(instanceId, [['LRANGE', key, start, end]]).subscribe(ret => {
           this.data = injectValuesToArray(ret[0]);
           this.loadingPageData = false;
+          // this.showPagination = true;
         }
       );
     } else if (type === 'zset') {
@@ -184,6 +187,7 @@ export class DataViewerComponent implements OnInit, OnChanges {
             });
             i += 2;
             this.loadingPageData = false;
+            this.showPagination = true;
           }
         }
       );
@@ -198,6 +202,7 @@ export class DataViewerComponent implements OnInit, OnChanges {
       } else {
         this.data = this.setCachedData.slice(start, end);
       }
+      this.showPagination = true;
     } else if (type === 'hash') {
       if (!this.hashCachedData) {
         this.loadingPageData = true;
@@ -217,6 +222,7 @@ export class DataViewerComponent implements OnInit, OnChanges {
       } else {
         this.data = this.hashCachedData.slice(start, end);
       }
+      this.showPagination = true;
     }
   }
 
